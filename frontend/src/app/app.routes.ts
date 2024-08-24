707f01f687dc96afcee9c0bfb432bd46c52ac33b
Alexander Schleich <d3athwing@web.de>
2024-08-24 20:43:46 +0200

    * Default Change-Detection for generated components set to "OnPush" (for better Change-Detection performance) * Generated components are generated as "standalone" (This is the new recommended style) * Added a very basic layout component which can act as the main "frame" around the content. (Contains Header and Navigation) * Added a simple Routing example for 2 components which are being displayed in the content area

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { ContentAComponent } from './content-a/content-a.component';
+import { ContentBComponent } from './content-b/content-b.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'component-a',
+        component: ContentAComponent
+      },
+      {
+        path: 'component-b',
+        component: ContentBComponent
+      }
+    ]
+  }
+];
---
+++ frontend/src/app/content-a/content-a.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-a',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-a.component.html',
+  styleUrl: './content-a.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ContentAComponent {
+
+}
