8df315a80c16f3df6ada3088809b8a4bf0317622
chino <chinodelmundo@gmail.com>
2024-08-30 15:22:27 +0800

    feat: put router-outlet to main-layout

--- frontend/src/app/main-layout/main-layout.component.ts
+++ frontend/src/app/main-layout/main-layout.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from '../components/header';
 import { SidebarComponent } from '../components/sidebar';
@@ -6,7 +7,7 @@ import { SidebarComponent } from '../components/sidebar';
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
