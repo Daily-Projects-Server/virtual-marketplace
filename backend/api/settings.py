0f6d52e4bcd07673aad1eb44600335b3658aae27
Liliane Lukong <liliane.haniel@gmail.com>
2024-09-01 16:50:52 +0100

    Implement Update Permissions for Listings for Owner vs Non-Owner Update Cases, Pytests, admin setup enhancements (#52)
    * Implement update permissions for listings and add tests for owner vs non-owner update scenarios.
    
    * Implement update permissions for listings and add tests for owner vs non-owner update scenarios.
    
    * resolved migration branch conflicts

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -38,7 +38,8 @@ env.escape_proxy = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] #This is safe and convenient for testing locally (Development Environment). In a production environment, you should include the domain names or IP addresses where your application will be hosted. Using a wildcard is not recommended for production environments because it makes your site more vulnerable to HTTP Host header attacks.
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+SITE_ID = 1
 
 
 
@@ -205,3 +206,6 @@ LOGGING = {
         },
     },
 }
+
+
+
--- backend/listings/models.py
+++ backend/listings/models.py
@@ -14,6 +14,13 @@ class Listing(BaseModel):
     owner = models.ForeignKey('users.User', on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  # Track when the listing was created
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('sold', 'Sold'),
+        ('archived', 'Archived'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')  # Track the status of the listing
 
     class Meta:
         verbose_name = "Listing"
