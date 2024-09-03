cbf1b469008b27214ddf588f4cb038d161680050
Burbudol <25privetik2016@gmail.com>
2024-09-03 21:50:22 +0300

    Edit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,26 +1,4 @@
 import { Routes } from '@angular/router';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { ContentAComponent } from './content-a/content-a.component';
-import { ContentBComponent } from './content-b/content-b.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: MainLayoutComponent,
-    children: [
-      {
-        path: 'component-a',
-        component: ContentAComponent
-      },
-      {
-        path: 'component-b',
-        component: ContentBComponent
-      }
-    ]
-  }
-];
-
-import { Routes } from '@angular/router';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { MainComponent } from './components/main/main.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
