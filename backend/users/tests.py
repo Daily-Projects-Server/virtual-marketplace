87fc1b3cf12b5ef9c204b52b31c79c224e3a6118
sunr4y <109840029+sunr4y@users.noreply.github.com>
2024-09-22 05:38:15 +0200

    Add CORS header tests

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,11 +1,15 @@
 import pytest
 from django.urls import reverse
+from django.test import TestCase
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.exceptions import TokenError
+from django.conf import settings
+
 from listings.models import Listing, Category
 from .models import Settings
+
 
 User = get_user_model()
 
@@ -338,3 +342,39 @@ class TestReviewViews:
         review = response.data
         response = client.delete(f'{review_url}{review["id"]}/')
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+class TestCORSHeaders:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, request):
+        self.client = APIClient()
+
+    @pytest.mark.django_db
+    def test_cors_headers(self):
+        url = reverse('login')
+        response = self.client.options(url, HTTP_ORIGIN='http://localhost:3000')
+
+        assert response['Access-Control-Allow-Origin'] == 'http://localhost:3000'
+        assert 'POST' in response['Access-Control-Allow-Methods']
+        assert 'authorization' in response['Access-Control-Allow-Headers'].lower()
+
+    @pytest.mark.django_db
+    def test_cors_allowed_origins(self):
+        url = reverse('login')
+        for origin in settings.CORS_ALLOWED_ORIGINS:
+            response = self.client.options(url, HTTP_ORIGIN=origin)
+            assert response['Access-Control-Allow-Origin'] == origin
+
+    @pytest.mark.django_db
+    def test_cors_disallowed_origin(self):
+        url = reverse('login')
+        disallowed_origin = 'http://example.com'
+        response = self.client.options(url, HTTP_ORIGIN=disallowed_origin)
+        
+        assert 'Access-Control-Allow-Origin' not in response
+
+    def test_cors_settings(self):
+        assert 'corsheaders' in settings.INSTALLED_APPS
+        assert 'corsheaders.middleware.CorsMiddleware' in settings.MIDDLEWARE
+        assert hasattr(settings, 'CORS_ALLOWED_ORIGINS')
+        assert isinstance(settings.CORS_ALLOWED_ORIGINS, list)
+        assert len(settings.CORS_ALLOWED_ORIGINS) > 0
